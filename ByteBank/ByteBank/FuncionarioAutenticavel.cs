5f2f404520e4033996ede39f7f8dc07473ed0d99
Juarez <juniorfatene@gmail.com>
2021-11-22 20:54:36 -0300

    adicionando outras classes

---
+++ ByteBank/ByteBank/FuncionarioAutenticavel.cs
@@ -0,0 +1,20 @@
+﻿using ByteBank.Funcionarios;
+using ByteBank.Sistemas;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ByteBank
+{
+	public abstract class FuncionarioAutenticavel : Funcionario, IAutenticavel
+	{
+		public FuncionarioAutenticavel(double salario,string cpf) : base(salario,cpf)
+		{
+		}
+		public string Senha { get; set; }
+		public bool Autenticar(string senha)
+		{
+			return Senha == senha;
+		}
+	}
+}
--- ByteBank/ByteBank/Program.cs
+++ ByteBank/ByteBank/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ByteBank.Funcionarios;
+using ByteBank.Sistemas;
+using System;
 
 namespace ByteBank
 {
@@ -6,7 +8,33 @@ namespace ByteBank
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			UsarSistema();
+			Console.ReadLine();
+		}
+
+		public static void UsarSistema()
+		{
+			SistemaInterno sistemaInterno = new SistemaInterno();
+
+			Diretor roberta = new Diretor("141.969.888-99");
+			roberta.Nome = "Roberta";
+			roberta.Senha = "123";
+
+			GerenteDeConta camila = new GerenteDeConta("145.256.414-88");
+			camila.Nome = "Camila";
+			camila.Senha = "abc";
+
+			ParceiroComercial parceiro = new ParceiroComercial();
+			parceiro.Senha = "123456";
+
+
+			Funcionario pedro = new Designer("833.222.048-39");
+			pedro.Nome = "Pedro";
+
+			sistemaInterno.Logar(parceiro, "123456");
+			//sistemaInterno.Logar(pedro, "123");
+			sistemaInterno.Logar(roberta, "123");
+			sistemaInterno.Logar(camila, "abc");
 		}
 	}
 }
